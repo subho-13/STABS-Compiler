af79ed5ae086b922da326ad7e28c66fb8e789416
Soumalya1857 <soumalyakundu10@gmail.com>
2021-05-14 08:49:39 +0530

    Terminal.java updated to include all terminals

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This branch is for Arko and soumalya!<br>
 Arko => Anything you get meaningful dump here
 
+### Some important points:
+* We don't need to worry about Non-Terminal class! That's a part of parser .
+
+
 ### Some articles may help:
 * [Starting a basic lexical analyzer](https://stackoverflow.com/questions/17848207/making-a-lexical-analyzer)
--- src/terminal/Terminal.java
+++ src/terminal/Terminal.java
@@ -1,7 +1,70 @@
 package terminal;
 
 public enum Terminal {
+    /*data types*/
     INT, FLOAT,
+
+    /*value identifiers*/
     INT_VAL, FLOAT_VAL, IDENTIFIER,
+
+    /*all paranthesis*/
+    LEFT_PARA, RIGHT_PARA, LEFT_CURLY, RIGHT_CURLY, LEFT_SQ, RIGHT_SQ,
+
+    /*all comparison operators*/
+    EQUALS, NOT_EQ, LESSER, LESSER_EQ, GREATER, GREATER_EQ,
+
+    /*conjunctives*/
+    AND, OR,
+
+    /*operators*/
+    INCREMENT, DECREMENT, ADD, SUBSTRACT, DIVIDE, MULTIPLY,
+
+    /*conditional branching and looping*/
+    IF, ELSE, FOR,
+
+    /*io options*/
+    PRINT, INPUT,
+
+    /*end line or statement delimeter*/
+    COMMA, SEMICOLON,
+
+    /*end of file*/
     EOF;
 }
+
+/*
++ NCREMENT = ++
++DECREMENT = --
++MULTIPLY = *
++DIVIDE = /
++ADD = +
++SUBSTRACT = -
++EQUALS = ==
++NOT_EQ = !=
++LESSER = <
++LESSER_EQ = <=
++GREATER = >
++GREATER_EQ = >=
++AND = ???????????????????????????????
++OR = ||
++ASSIGN = =
++LEFT_PARA = (
++RIGHT_PARA = )
++LEFT_CURLY = {
++RIGHT_CURLY = }
++LEFT_SQ = [
++RIGHT_SQ = ]
++SEMICOLON = ;
++COMMA = ,
++EOF = eof
++INT = int
++FLOAT = float
++IF = if
++ELSE = else
++FOR = for
++PRINT = print
++INPUT = input
++INT_VAL = [-+]?\d+
++FLOAT_VAL = [-+]?\d*.\d+
++IDENTIFIER = \w+
+*/
