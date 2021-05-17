08db0605cabd30a5793b0550670bef471f05e72a
subho-13 <souvikdutta6635h@gmail.com>
2021-05-17 14:49:15 +0530

    Updated

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,14 +1,23 @@
 package lexer;
 
+import lexer.lexerstate.LexerState;
 import lexer.scanner.Scanner;
 import token.Token;
 
 public class Lexer {
     private Token currentToken;
     private Scanner scanner;
+    private LexerState startState;
 
     public void parseNextToken() {
+        String string = scanner.getCurrentString();
+        LexerState currentState = startState;
 
+        for(char c: string.toCharArray()) {
+            currentState = currentState.move(c);
+        }
+
+        currentToken = currentState.getToken(string);
     }
 
     public Token getCurrentToken() {
--- src/lexer/lexerstate/LexerStateBuilder.java
+++ src/lexer/lexerstate/LexerStateBuilder.java
@@ -1,23 +1,34 @@
 package lexer.lexerstate;
 
+import lexer.Lexer;
 import terminal.Terminal;
 import token.ReservedToken;
 import token.Token;
+import token.valuedtoken.IntToken;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class LexerStateBuilder {
     LexerState start = new LexerState();
     LexerState state_1 = new LexerState();
-    LexerState state_35 = new LexerState();
-    LexerState state_43 = new LexerState();
+    LexerState state_2 = new LexerState();
+    LexerState state_3 = new LexerState();
 
-    private void configureStart() {
+    public LexerState getInitialState() {
+        return start;
     }
 
-    private void configureState35() {
-        state_35.setTokenBuilder((s) -> {
-            Token token = new ReservedToken(Terminal.IF);
+    private void configureStart() {
+        Map<Character, LexerState>  map = new HashMap<Character, LexerState>();
+        start.setTransitionMap(map);
+    }
+
+    private void configureState_1() {
+        Map<Character, LexerState>  map = new HashMap<Character, LexerState>();
+        state_1.setTransitionMap(map);
+        state_1.setTokenBuilder((string) -> {
+            Token token = new IntToken(Integer.parseInt(string));
             return token;
         });
     }
