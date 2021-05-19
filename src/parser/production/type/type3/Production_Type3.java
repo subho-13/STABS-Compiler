b7efd3fa8c2cab473ce2bb93832918f520569d2b
subho-13 <souvikdutta6635h@gmail.com>
2021-05-19 16:08:21 +0530

    FunctionTable

--- src/parser/production/type/type3/Production_Type3.java
+++ src/parser/production/type/type3/Production_Type3.java
@@ -2,14 +2,16 @@ package parser.production.type.type3;
 
 import parser.Parser;
 import parser.parserstack.ParserStack;
+import parser.production.Production;
 import parser.symboltable.SymbolTable;
+import parser.table.FunctionTable;
 
-public class Production_Type3 {
+public class Production_Type3 implements Production {
     ParserStack stack;
     FunctionTable table;
     Type_3 action;
 
-    public Production_Type3(ParserStack stack, SymbolTable table, Type_3 action){
+    public Production_Type3(ParserStack stack, FunctionTable table, Type_3 action){
         this.stack  = stack;
         this.table = table;
         this.action = action;
@@ -17,7 +19,6 @@ public class Production_Type3 {
     @Override
     public void applyRule(){
         action.use(stack, table);
-
     }
 
 
--- src/parser/production/type/type3/Type_3.java
+++ src/parser/production/type/type3/Type_3.java
@@ -2,6 +2,7 @@ package parser.production.type.type3;
 
 import parser.Parser;
 import parser.parserstack.ParserStack;
+import parser.table.FunctionTable;
 
 public interface Type_3 {
     public void use(ParserStack stack, FunctionTable table);
