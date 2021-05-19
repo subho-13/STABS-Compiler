d34e76178372a364801cedf5adf808d9210ec43e
Titir Adhikary <titir.adhikary@hotmail.com>
2021-05-19 16:06:45 +0530

    Updated the class of Type_3

--- src/parser/production/builder/type3/Builder_Type3.java
+++ src/parser/production/builder/type3/Builder_Type3.java
@@ -3,16 +3,18 @@ package parser.production.builder.type3;
 import jdk.incubator.foreign.LibraryLookup;
 import parser.parserstack.ParserStack;
 import parser.production.type.type2.Production_Type2;
-import parser.symboltable.SymbolTable;
+import parser.production.type.type3.Production_Type3;
+import parser.table.FunctionTable;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Builder_Type3 {
     private ParserStack stack;
     private FunctionTable table;
     private Map<Integer, Production_Type3> map = new HashMap<>();
 
-    public Builder_Type3(ParserStack stack, SymbolTable table){
+    public Builder_Type3(ParserStack stack, FunctionTable table){
         this.stack = stack;
         this.table = table;
         setAllProductions();
--- src/parser/production/type/type3/Production_Type3.java
+++ src/parser/production/type/type3/Production_Type3.java
@@ -2,14 +2,15 @@ package parser.production.type.type3;
 
 import parser.Parser;
 import parser.parserstack.ParserStack;
-import parser.symboltable.SymbolTable;
+import parser.production.Production;
+import parser.table.FunctionTable;
 
-public class Production_Type3 {
+public class Production_Type3 implements Production {
     ParserStack stack;
     FunctionTable table;
     Type_3 action;
 
-    public Production_Type3(ParserStack stack, SymbolTable table, Type_3 action){
+    public Production_Type3(ParserStack stack, FunctionTable table, Type_3 action){
         this.stack  = stack;
         this.table = table;
         this.action = action;
