ef9e2d18fecfe2ad61fa98498142fda4bb86a742
Arkojyoti Sen <arkosen441@gmail.com>
2021-05-20 03:02:04 +0530

    Bye bye

---
+++ src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import lexer.Lexer;
+import lexer.lexerstate.LexerStateBuilder;
+import lexer.lexerstate.StateMapping;
+import lexer.scanner.Scanner;
+import parser.Parser;
+import parser.driver.Driver;
+import parser.parserstack.ParserStack;
+import parser.production.builder.ProductionBuilder;
+import parser.synchronizer.Synchronizer;
+import parser.table.FunctionTable;
+import parser.table.SymbolTable;
+import util.ParseTree.ParseTree;
+
+import java.io.File;
+
+public class Main {
+    public static  void main(String[] args) {
+        File file = new File("src/test/testfiles/File1.stabs");
+        try {
+            Scanner scanner = new Scanner(file);
+            LexerStateBuilder stateBuilder = new LexerStateBuilder(new StateMapping());
+            Lexer lexer = new Lexer(scanner, stateBuilder);
+            SymbolTable symbolTable = new SymbolTable();
+            FunctionTable functionTable = new FunctionTable();
+            ParseTree parseTree = new ParseTree();
+            ParserStack parserStack = new ParserStack(parseTree);
+            ProductionBuilder productionBuilder = new ProductionBuilder(parserStack, symbolTable, functionTable);
+            Driver driver = new Driver(productionBuilder);
+            Synchronizer synchronizer = new Synchronizer();
+            Parser parser = new Parser(parserStack, lexer, driver, synchronizer);
+            parser.parse();
+            if (functionTable.hasUnresolvedFunctionNames()) {
+                throw new Exception("The following functions are unresolved :: " + functionTable.unresolvedFunctionNames());
+            }
+            parseTree.display();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
